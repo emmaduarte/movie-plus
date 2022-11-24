d4d07b5c2d5d06366537f680e3ce55ee4803c574
emmaduarte <emma.ale.duarte@gmail.com>
2022-11-24 02:32:15 -0300

    Actualizacion de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Listado from './Componentes/Listado/Listado'
 import Detalles from './Componentes/Detalles/Detalles'
 import Nav from './Componentes/Nav/Nav'
-import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 const App = () => {
   
@@ -12,18 +12,18 @@ const App = () => {
       
     
      <div>
-      <BrowserRouter basename="/movie-plus">
+      
       <Router>
         <Nav/>
           <Routes>
             
             <Route path='/movie-plus' exact element={<Listado/>}/>
-            <Route path='/movie-plus/Detalles/:title/:id' exact element={<Detalles/>}/>
+            <Route path='/Detalles/:title/:id' exact element={<Detalles/>}/>
             
           </Routes>
         
       </Router>
-      </BrowserRouter>
+      
       </div>
       
 </>
