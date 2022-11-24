74802c3763fc5c8c11f9a14294cc64683b6385d1
emmaduarte <emma.ale.duarte@gmail.com>
2022-11-24 00:23:50 -0300

    Actualizacion de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
      <HashRouter>
       <Router>
         <Nav/>
+        <Listado/>
           <Routes>
             
             <Route path='/' exact element={<Listado/>}/>
