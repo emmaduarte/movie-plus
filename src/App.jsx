3c2e6c3598b0d8440bf4884e3f29c0e1ff942bac
emmaduarte <emma.ale.duarte@gmail.com>
2022-11-24 00:53:10 -0300

    Actualizacion de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <>
       
     
-     
+     <HashRouter>
       <Router>
         <Nav/>
           <Routes>
@@ -21,6 +21,7 @@ const App = () => {
           </Routes>
         
       </Router>
+      </HashRouter>
       
 </>
       
