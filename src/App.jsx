aabb668234a3bc5de3454e0a8a7f203e37f3957c
emmaduarte <emma.ale.duarte@gmail.com>
2022-11-24 00:11:26 -0300

    Actualizacion de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Listado from './Componentes/Listado/Listado'
 import Detalles from './Componentes/Detalles/Detalles'
 import Nav from './Componentes/Nav/Nav'
-import {BrowserRouter as Router, HashRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 const App = () => {
   
@@ -16,8 +16,8 @@ const App = () => {
         <Nav/>
           <Routes>
             
-            <Route path='https://emmaduarte.github.io/movie-plus/' exact element={<Listado/>}/>
-            <Route path='https://emmaduarte.github.io/movie-plus/Detalles/:title/:id' exact element={<Detalles/>}/>
+            <Route path='/' exact element={<Listado/>}/>
+            <Route path='/Detalles/:title/:id' exact element={<Detalles/>}/>
           </Routes>
         
       </Router>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { HashRouter } from 'react-router-dom'
 import App from "./App"
 import './index.css'
 
-ReactDom.render(<App/>, document.querySelector("#root"))
+ReactDom.render(
+    <React.StrictMode>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </React.StrictMode>,
+document.querySelector("#root"));
